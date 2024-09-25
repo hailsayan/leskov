9647fed6f2427b3a43b0fc682e284c4971fbf309
Psyon <psyon.dev@gmail.com>
2024-09-25 18:37:29 +0330

    feat: Add MySQL database integration

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,27 @@ package main
 import (
 	"log"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/hailsayan/Go-Rest/cmd/api"
+	"github.com/hailsayan/Go-Rest/config"
+	"github.com/hailsayan/Go-Rest/db"
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := api.NewAPIServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
