8d76381e0df73f7e0ac6b9392d7b01febc89c89a
Psyon <psyon.dev@gmail.com>
2024-09-26 15:17:57 +0330

    feat: Establish database connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/go-sql-driver/mysql"
@@ -22,9 +23,18 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	initStorage(db)
 
 	server := api.NewAPIServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("DB: Successfully connected!")
 }
